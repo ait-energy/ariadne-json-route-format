237cec61b75ed9822ca6591def27cdae8ea29ed0
Markus Straub <markus.straub@ait.ac.at>
2016-07-29 10:54:24 +0200

    properly implemented Landmark

--- src/main/java/at/ac/ait/ariadne/routeformat/instruction/Landmark.java
+++ src/main/java/at/ac/ait/ariadne/routeformat/instruction/Landmark.java
@@ -1,23 +1,63 @@
 package at.ac.ait.ariadne.routeformat.instruction;
 
+import at.ac.ait.ariadne.routeformat.location.Location;
+
 /**
- * TODO replace with a new class extending Location?
- * 
  * @author AIT Austrian Institute of Technology GmbH
  */
 public class Landmark {
 
-	public enum Preposition {
-		BEFORE, AT, AFTER
-	}
+    public enum Preposition {
+        BEFORE, AT, AFTER
+    }
 
-	public final Preposition preposition;
-	public final String description;
+    private final Preposition preposition;
+    private final Location location;
 
-	public Landmark(Preposition preposition, String description) {
-		super();
-		this.preposition = preposition;
-		this.description = description;
-	}
+    public Preposition getPreposition() {
+        return preposition;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    private Landmark(Builder builder) {
+        super();
+        this.preposition = builder.preposition;
+        this.location = builder.location;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static Builder builder(Preposition preposition, Location location) {
+        return new Builder().withPreposition(preposition).withLocation(location);
+    }
+
+    @Override
+    public String toString() {
+        return "Landmark [preposition=" + preposition + ", location=" + location + "]";
+    }
+
+    public static class Builder {
+        private Preposition preposition;
+        private Location location;
+
+        public Builder withPreposition(Preposition preposition) {
+            this.preposition = preposition;
+            return this;
+        }
+
+        public Builder withLocation(Location location) {
+            this.location = location;
+            return this;
+        }
+
+        public Landmark build() {
+            return new Landmark(this);
+        }
+    }
 
 }
